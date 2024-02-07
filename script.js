b325ba7026686f4fafbbd601f832c26b24e24f05
Shashank Tripathi <56106924+shankhub01@users.noreply.github.com>
2024-02-07 20:36:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const boldElement = document.querySelector(".para");
+const container= document.getElementById("container");
+function enterBtn(){
+	boldElement.remove();
+	let element = document.createElement("h1");
+	element.innerText="Entered Metaverse";
+	container.appendChild(element);
+	
+}
